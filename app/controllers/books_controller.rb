7d4c7fa17a01b334615834fb6fab78094f12ffdf
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2024-11-07 13:58:37 +0000

    [start]destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,10 +3,10 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-  def Create
+  def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/top'
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -14,10 +14,25 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book =Book.find(params[:id])
   end
 
   def edit
+    @book =Book.find(params[:id])
   end
+
+  def update
+    book =Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
 
 private
 def book_params
