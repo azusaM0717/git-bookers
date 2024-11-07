ea821f190c99ba594b2460464bf4dd9ae2a05afc
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2024-11-07 12:08:07 +0000

    [start]新規登録機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,27 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
 
+  def Create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+  end
+
+  def index
+    @books =Book.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+private
+def book_params
+  params.require(:book).permit(:title,:body,:image)
+end
 
 end
