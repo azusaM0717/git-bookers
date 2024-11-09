e8a7d9fa27178740107cc56168a332a428e25e7a
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2024-11-09 23:37:38 +0000

    [start]validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,8 @@ class BooksController < ApplicationController
       flash[:notice] ="Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      redirect_to books_path
+      @books = Book.all
+      render :index
     end
   end
 
