50a1d8157c77aca3274dc4734d4d7201c4a07b54
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2024-11-09 12:18:31 +0000

    [start]validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,10 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book.id), notice:"Book was successfully created."
+      flash[:notice] ="Book was successfully created."
+      redirect_to book_path(@book.id)
     else
-      render 'books'
+      redirect_to books_path
     end
   end
 
@@ -25,10 +26,15 @@ class BooksController < ApplicationController
   end
 
   def update
-    book =Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book =Book.find(params[:id])
+      if @book.update(book_params)
+      flash[:notice] ="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
+    
 
   def destroy
     book = Book.find(params[:id])
@@ -39,6 +45,6 @@ class BooksController < ApplicationController
 
 private
   def book_params
-    params.require(:book).permit(:title,:body)
+    params.require(:book).permit(:title, :body)
   end
 end
